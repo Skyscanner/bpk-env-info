5e905c28cfa46dc426ff59c3ba760c628128e15a
Hugo Tunius <hugo.tunius@skyscanner.net>
2018-05-08 14:56:28 +0100

    Improvements

---
+++ README.md
@@ -0,0 +1,9 @@
+# bpk-env-info
+
+Print environment information about a [Backpack](http://github.com/skyscanner/backpack) project.
+
+## Usage
+
+```bash
+npx bpk-env-info
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
+
+import { minify } from 'uglify-es';
+import uglify from 'rollup-plugin-uglify';
+import shebang from 'rollup-plugin-shebang';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs',
   },
+  external: ['child_process', 'util'],
+  plugins: [
+    commonjs(),
+    uglify({}, minify),
+    shebang(),
+  ],
 };
